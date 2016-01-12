caa5ff9212bb0e54e0500878ff9e24df46d6f5f7
root <root@>
2016-01-13 00:05:00 +0100

    isolation d'acces au donnees dans un fichier

--- index.php
+++ index.php
@@ -5,39 +5,5 @@
 					'POST_TITLE as title,POST_CONTENT as content FROM T_POST'.
 					' ORDER BY POST_ID DESC');
 				$posts->setFetchMode(PDO::FETCH_OBJ);
-				foreach ($posts as $post) : 
-?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>My Blog</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-	<div id="global">
-		<header>
-			<a href="index.php">
-				<h1 id="title"></h1></a>
-			<p>description</p>
-		</header>
-		<div class="content">
-
-			<article>
-				<header>
-					<p><?php echo $post->title; ?></p>
-					<time><?php echo $post->date; ?></time>
-				</header>		
-				<p><?php echo $post->content; ?></p>
-			</article>
-			<hr>
-			<?php endforeach; ?>
-			</div><!-- content -->
-			 <footer>
-			 	<p>Blog created in CSS, PHP, HTML5</p>
-			 </footer>
-	</div><!-- global -->
-</body>
-</html>
+				
+				require 'vueAccueil.php';
---
+++ vueAccueil.php
@@ -0,0 +1,34 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>My Blog</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id="global">
+		<header>
+			<a href="index.php">
+				<h1 id="title"></h1></a>
+			<p>description</p>
+		</header>
+		<div class="content">
+		<?php foreach ($posts as $post) : ?>
+			<article>
+				<header>
+					<p><?php echo $post->title; ?></p>
+					<time><?php echo $post->date; ?></time>
+				</header>		
+				<p><?php echo $post->content; ?></p>
+			</article>
+			<hr>
+			<?php endforeach; ?>
+			</div><!-- content -->
+			 <footer>
+			 	<p>Blog created in CSS, PHP, HTML5</p>
+			 </footer>
+	</div><!-- global -->
+</body>
+</html>
